6d88809ddacb1581c125f09a66dd1a0025d32138
MTeresaMB <mariateresa.milanes.alu@iescampanillas.com>
2022-03-23 22:01:13 +0100

    updte

--- Modulo 5 - Laboratorio - Arrays/script.js
+++ Modulo 5 - Laboratorio - Arrays/script.js
@@ -28,53 +28,68 @@ const carrito = [{
   },
 
 ];
-window.addEventListener('DOMContentLoaded', function () {
-  let compra = 0;
-  let buttonCesta = document.getElementById('carrito');
-  buttonCesta.addEventListener('click', addElement, false);
 
-
-  function addElement() {
-    let tabla = document.getElementById('tabla');
-    tabla.style.display = 'visible';
-    let thead = document.createElement('thead');
-    let filaThead = document.createElement('tr');
-    let thPro = document.createElement('th');
-    let thPrecio = document.createElement('th');
-    let thCantidad = document.createElement('th');
-    let thTotal = document.createElement('th');
-    
-    thPro.innerHTML = 'Producto';
-    thPrecio.innerHTML = 'Precio';
-    thCantidad.innerHTML = 'Cantidad';
-    thTotal.innerHTML = 'Total';
-
-    tabla.appendChild(thead);
-    thead.appendChild(thPro);
-    thead.appendChild(thPrecio);
-    thead.appendChild(thCantidad);
-    thead.appendChild(thTotal);
-    thead.appendChild(filaThead);
-    
-
-    while (compra < carrito.length) {
-      let fila = document.createElement('tr');
-      let nombre = document.createElement('td');
-      let precio = document.createElement('td');
-      let cantidad = document.createElement('td');
-      let totalProducto = document.createElement('td');
-      
-      nombre.innerHTML = carrito[compra].name;
-      precio.innerHTML = carrito[compra].price + "€";
-      cantidad.innerHTML = carrito[compra].count;
-      totalProducto.innerHTML = Number((carrito[compra].price * carrito[compra].count).toFixed(2)) + "€";
-
-      fila.appendChild(nombre);
-      fila.appendChild(precio);
-      fila.appendChild(cantidad);
-      fila.appendChild(totalProducto);
-      tabla.appendChild(fila);
-      compra++;
+let compra = 0;
+let buttonCesta = document.getElementById('carrito');
+let buttonListado = document.getElementById('listado');
+let buttonBorrado = document.getElementById('borrado');
+buttonCesta.addEventListener('click', addElement);
+buttonListado.addEventListener('click', listadoProductos);
+buttonBorrado.addEventListener('click', function(){
+  let indice;
+  for(let i = 0; i < carrito.length; i++){
+    if(carrito[i].id == 54657){
+      indice = i;
     }
   }
+  carrito.splice(indice, 1);
+  listadoProductos();
 });
+
+
+function addElement() {
+  tabla.style.display = 'block';
+  while (compra < carrito.length) {
+    let fila = document.createElement('tr');
+    let nombre = document.createElement('td');
+    let precio = document.createElement('td');
+    let cantidad = document.createElement('td');
+    let totalProducto = document.createElement('td');
+
+    nombre.innerHTML = carrito[compra].name;
+    precio.innerHTML = carrito[compra].price + "€";
+    cantidad.innerHTML = carrito[compra].count;
+    totalProducto.innerHTML = Number((carrito[compra].price * carrito[compra].count).toFixed(2)) + "€";
+
+    fila.appendChild(nombre);
+    fila.appendChild(precio);
+    fila.appendChild(cantidad);
+    fila.appendChild(totalProducto);
+    tabla.appendChild(fila);
+    compra++;
+  };
+};
+
+function listadoProductos() {
+  tabla.style.display = 'block';
+  while (compra < carrito.length) {
+    let fila = document.createElement('tr');
+    let nombre = document.createElement('td');
+    let precio = document.createElement('td');
+    let cantidad = document.createElement('td');
+    let totalProducto = document.createElement('td');
+
+    nombre.innerHTML = carrito[compra].name;
+    precio.innerHTML = carrito[compra].price + "€";
+    cantidad.innerHTML = '';
+    totalProducto.innerHTML = '';
+
+    fila.appendChild(nombre);
+    fila.appendChild(precio);
+    fila.appendChild(cantidad);
+    fila.appendChild(totalProducto);
+    tabla.appendChild(fila);
+    compra++;
+  };
+}
+
