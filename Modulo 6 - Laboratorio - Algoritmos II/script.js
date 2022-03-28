54ffa2d94831340b857d303e2037be7b5ce550c8
MTeresaMB <mariateresa.milanes.alu@iescampanillas.com>
2022-03-28 20:13:45 +0200

    update

---
+++ Modulo 6 - Laboratorio - Algoritmos II/script.js
@@ -0,0 +1,54 @@
+var plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
+var encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
+
+
+
+var btnEncrypted = document.getElementById('btnEncrypt').addEventListener('click', () =>{
+  var texto = document.getElementById('encrypt').value;
+  var decrypt = document.getElementById("decrypt");
+  decrypt.innerHTML = transformMessage(texto);
+  
+});
+
+var btnDecrypted = document.getElementById('btnDecrypt').addEventListener('click', () =>{
+  var texto2 = document.getElementById('decrypt').value;
+  var encrypt = document.getElementById("encrypt");
+  encrypt.innerHTML = transformMessageInverse(texto2);
+})
+
+
+
+var searchIndex = (alphabet, letter) => {
+  for(var i = 0; i < alphabet.length; i++){
+    if(alphabet[i] === letter) return i;
+  }
+  return -1;
+}
+
+var transformLetter = (letter, origin, destiny) => {
+  var letterIndex = searchIndex(origin, letter);
+  if(letterIndex === -1){
+    return letter;
+  }else{
+    return destiny[letterIndex];
+  } 
+}
+
+var transformMessage = message => {
+  var messageLower = message.toLowerCase();
+  var result = "";
+  for(var letter of messageLower){
+    result = result + transformLetter(letter, plainAlphabet, encryptedAlphabet );
+  }
+  return result;
+}
+
+
+var transformMessageInverse = message => {
+  var messageLower = message.toLowerCase();
+  var result = "";
+  for(var letter of messageLower){
+    result = result + transformLetter(letter, encryptedAlphabet, plainAlphabet );
+  }
+  return result;
+}
