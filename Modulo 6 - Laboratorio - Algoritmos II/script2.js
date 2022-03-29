199b45808e742dd80a9a5bbea41bb45164197a42
MTeresaMB <mariateresa.milanes.alu@iescampanillas.com>
2022-03-29 16:31:55 +0200

    update

--- Modulo 6 - Laboratorio - Algoritmos II/script2.js
+++ Modulo 6 - Laboratorio - Algoritmos II/script2.js
@@ -1,19 +1,28 @@
 /**
- * Ejercicio numeros aleatorios
+ * Inserta numeros aleatorios en un array sin repetir
  */
-
-
+'use strict';
 
 function randomPick(n, min, max){
-  var arrAle = [];
-  for(var i = 1; i <= n; i++){
-    arrAle.push(Math.floor(Math.random() * (max - min) + min));
+  let arrAle = [];
+  let repeat = false;
+  for(let i = 0; i < n; i++){
+    do{
+      repeat = false;
+      let numAle = Math.floor(Math.random() * (max - min) + min);
+      if(arrAle.includes(numAle)){ 
+        repeat = true;
+      }
+      if(repeat == false){
+        arrAle.push(numAle);
+      }
+    }while(repeat == true);
   }
   console.log(arrAle);
 }
 randomPick(6, 1, 49);
-randomPick(15, 1 , 15);
-randomPick(1, 1, 6);
+//randomPick(15, 1 , 15); al activar esta funcion sale un bucle infinito, no se porque lo hace, he revisado el codigo toda la mañana y no encuentro el error 
+ randomPick(1, 1, 6);
 
 
 
