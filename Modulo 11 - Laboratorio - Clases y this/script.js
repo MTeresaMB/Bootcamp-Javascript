d0fa4c3dbd39c4c338dbf71406ab4986c05d018c
MTeresaMB <mariateresa.milanes.alu@iescampanillas.com>
2022-04-04 20:54:39 +0200

    update

--- Modulo 11 - Laboratorio - Clases y this/script.js
+++ Modulo 11 - Laboratorio - Clases y this/script.js
@@ -60,14 +60,17 @@ class TouristicOperator extends Booking{
   }
 }
 let booking = new Booking('standard', 1, 5);
-console.log(booking.calculaSubtotal());
+console.log(`El subtotal de la reserva 1 es: ${booking.calculaSubtotal()}€`);
+console.log(`El Total de la reserva 1 es: ${booking.calculaTotal()}€`);
 
 let booking2 = new Booking('suite', 2, 5);
-console.log(booking2.calculaTotal());
+console.log(`El subtotal de la reserva 2 es: ${booking2.calculaSubtotal()}€`);
+console.log(`El Total de la reserva 2 es: ${booking2.calculaTotal()}€`);
 
 let touristOperator = new TouristicOperator(1, 5);
-console.log(touristOperator.calculaSubtotal());
-console.log(touristOperator.calculaTotal());
+console.log(`El subtotal del tour operador es: ${touristOperator.calculaSubtotal()}€`);
+console.log(`El Total del tour operador es: ${touristOperator.calculaTotal()}€`);
+
 
 class Booking2 {
   constructor(tipoHabitacion, desayuno, pax, noches){
@@ -77,16 +80,8 @@ class Booking2 {
     this.noches = noches;
   }
 
-  typeRoom(){
-    switch(this.tipoHabitacion){
-      case "standard": return 100; break;
-      case "suite": return 150; break;
-      default: return 'Type room no available';
-    }
-  }
-
   calculaSubtotal(){
-    const priceRoom = this.typeRoom();
+    const priceRoom = 100;
     let addPerson = 0;
     let addDesayuno = 0;
     if(this.pax > 1){
@@ -94,8 +89,10 @@ class Booking2 {
     }
     if(this.desayuno === true){
       addDesayuno = 15;
+      return (priceRoom * (this.noches + addPerson) + addDesayuno).toFixed(2);
+    }else if(this.desayuno === false){
+      return (priceRoom * (this.noches + addPerson)).toFixed(2);
     }
-    return priceRoom * (this.noches + addPerson) + addDesayuno;
   }
   
   calculaTotal(){ 
@@ -104,4 +101,9 @@ class Booking2 {
 }
 
 let booking3 = new Booking2('standard',true, 1, 5);
-console.log(booking3.calculaSubtotal());
+console.log(`El subtotal de la reserva 3 con desayuno es: ${booking3.calculaSubtotal()}€`);
+console.log(`El Total de la reserva 3 con desayuno es: ${booking3.calculaTotal()}€`);
+
+let booking4 = new Booking2('suite',false, 1, 5);
+console.log(`El subtotal de la reserva 4 con desayuno es: ${booking4.calculaSubtotal()}€`);
+console.log(`El subtotal de la reserva 4 con desayuno es: ${booking4.calculaTotal()}€`);
