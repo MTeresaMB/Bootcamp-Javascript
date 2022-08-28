5764bffad6c06225e831ef29ced900143c03d6eb
Maria Teresa Milanés Barroso <72388130+MTeresaMB@users.noreply.github.com>
2022-08-28 18:53:14 +0200

    update validation date

--- Modulo 12 - Banca Online/src/pages/transfer/transfer.validations.js
+++ Modulo 12 - Banca Online/src/pages/transfer/transfer.validations.js
@@ -1,4 +1,4 @@
-import {Validators, createFormValidation} from '@lemoncode/fonk'
+import { Validators, createFormValidation } from '@lemoncode/fonk';
 import { ibanValidator } from './transfer.customValidator';
 import { laterDate } from '@lemoncode/fonk-later-date-validator';
 import { positiveNumber } from '@lemoncode/fonk-positive-number-validator';
@@ -8,78 +8,92 @@ const validationSchema = {
     email: [
       {
         validator: Validators.email,
-        message: 'Email no valido'
-      }
+        message: 'Email no valido',
+      },
     ],
     iban: [
       {
         validator: Validators.required,
-        message: 'Campo requerido'
+        message: 'Campo requerido',
       },
       {
         validator: ibanValidator,
-        customArgs:{
+        customArgs: {
           countryCode: 'ES',
-        }
+        },
       },
     ],
-    name:[
+    name: [
       {
         validator: Validators.required,
-        message: 'Campo requerido'
+        message: 'Campo requerido',
       },
     ],
-    amount:[
+    amount: [
       {
         validator: Validators.required,
-        message: 'Campo requerido'
+        message: 'Campo requerido',
       },
       {
         validator: positiveNumber.validator,
-        customArgs: { allowZero: true },
+        message: 'Introduzca una cantidad numérica positiva',
       },
     ],
-    concept:[
+    concept: [
       {
         validator: Validators.required,
-        message: 'Campo requerido'
+        message: 'Campo requerido',
       },
     ],
-    notes:[
+    notes: [
       {
         validator: Validators.required,
-        message: 'Campo requerido'
+        message: 'Campo requerido',
+      },
+    ],
+    day: [
+      {
+        validator: Validators.required,
+        message: 'Campo requerido',
+      },
+      {
+        validator: Validators.pattern,
+        customArgs: { pattern: /^([1-9]|[1-2]\d|3[01])$/ },
+        message: 'Introduzca un día válido',
+      },
+    ],
+    month: [
+      {
+        validator: Validators.required,
+        message: 'Campo requerido',
+      },
+      {
+        validator: Validators.pattern,
+        customArgs: { pattern: /^([1-9]|1[012])$/ },
+        message: 'Introduzca un mes válido',
+      },
+    ],
+    year: [
+      {
+        validator: Validators.required,
+        message: 'Campo requerido',
+      },
+      {
+        validator: Validators.pattern,
+        customArgs: { pattern: /^2[0-9][2-9](\d{1})$/ },
+        message: 'Introduzca un año válido',
       },
     ],
     date: [
-      // {
-      //   validator: laterDate.validator,
-      //   customArgs:
-      //     {
-      //       date: new Date(),
-      //       parseStringToDateFn: value => new Date(value)
-      //     }
-      // }
-    ],
-    day:[
       {
-        validator: Validators.required,
-        message: 'Campo requerido'
-      },
-
-    ],
-    month:[
-      {
-        validator: Validators.required,
-        message: 'Campo requerido'
+        validator: laterDate.validator,
+        customArgs: {
+          parseStringToDateFn: (value) => new Date(value),
+          date: new Date(),
+        },
+        message: 'Fecha no válida',
       },
     ],
-    year:[
-      {
-        validator: Validators.required,
-        message: 'Campo requerido'
-      },
-    ]
   },
 };
 
